80a7264d8fd8bd3b54f1a2f122d8effdff4b9752
Matthew Consterdine <mc21g14@soton.ac.uk>
2016-11-08 18:48:49 +0000

    Add generatelist() to create lists of names for pen and paper roleplay

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 // Elements
-var nametype = document.getElementsByClassName('nametype')[0];
+var nametype  = document.getElementsByClassName('nametype')[0];
 function getnamelist() {return document.getElementById(nametype.value);}
-var gender   = document.getElementById('gender');
-var quantity = document.getElementById('quantity');
-var output   = document.getElementById('output');
+var gender    = document.getElementById('gender');
+var quantity  = document.getElementById('quantity');
+var output    = document.getElementById('output');
+var seperator = "\n";
+var clear     = true;
 
 // Load cookie
 if(readCookie("selectedGenerator") != null) {
@@ -12,6 +14,25 @@ if(readCookie("selectedGenerator") != null) {
 	getnamelist().value = options[1];
 	gender.value        = options[2];
 	quantity.value      = options[3];
+}
+
+// Generate a list of names for pen and paper roleplay, you need to run this once to load the generators, and once to generate the names
+function generatelist() {
+	clear = false;
+	seperator = ", ";
+	output.innerText = "";
+	for(var i = 0; i < getnamelist().length; i++) {
+		getnamelist().selectedIndex = i;
+		output.innerText += "\n" + getnamelist().value.replace("_", " ") + "\n";
+		gender.value = "male";
+		generate();
+		output.innerText += "\n";
+		gender.value = "female";
+		generate();
+		console.log(i);
+	}
+	clear = true;
+	seperator = "\n";
 }
 
 // Generate names
@@ -24,15 +45,15 @@ function generate() {
 	var generator = window["generator$"+nametype.value.replace(/-/g, '_')+"$"+getnamelist().value];
 	if(typeof generator === 'function') {
 		output.setAttribute("data-generator", nametype.value+"$"+getnamelist().value);
-		output.innerText = "";
-		for(var i = 0; i < quantity.value; i++) output.innerText += generator(gender.value == "female" ? 1 : 0) + "\n";
+		if(clear) output.innerText = "";
+		for(var i = 0; i < quantity.value; i++) output.innerText += generator(gender.value == "female" ? 1 : 0) + seperator;
 	} else {
 		loadScript("generators/"+nametype.value+"/"+getnamelist().value+".min.js", function() {
 			var generator = window["generator$"+nametype.value.replace(/-/g, '_')+"$"+getnamelist().value];
 			if(typeof generator === 'function') {
 				output.setAttribute("data-generator", nametype.value+"$"+getnamelist().value);
-				output.innerText = "";
-				for(var i = 0; i < quantity.value; i++) output.innerText += generator(gender.value == "female" ? 1 : 0) + "\n";
+				if(clear) output.innerText = "";
+				for(var i = 0; i < quantity.value; i++) output.innerText += generator(gender.value == "female" ? 1 : 0) + seperator;
 			} else {
 				console.log("Not found: generator$"+nametype.value.replace(/-/g, '_')+"$"+getnamelist());
 			}
